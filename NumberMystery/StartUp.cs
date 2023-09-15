f0f42749db88f4762c898064387a28941f33e6c9
KTsaneff <78788769+KTsaneff@users.noreply.github.com>
2023-09-15 19:16:27 +0300

    Project files uploaded

---
+++ NumberMystery/StartUp.cs
@@ -0,0 +1,40 @@
+﻿namespace NumberMystery
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Random randomNumber = new();
+            int computerNumber = randomNumber.Next(1, 101);
+
+            while (true)
+            {
+                Console.Write($"Guess a number (1-100): ");
+                string playerInput = Console.ReadLine();
+
+                bool isValid = int.TryParse(playerInput, out int playersGuess);
+
+                if (isValid)
+                {
+                    if (playersGuess == computerNumber)
+                    {
+                        Console.WriteLine("You guessed it!");
+                        break;
+                    }
+                    else if (playersGuess > computerNumber)
+                    {
+                        Console.WriteLine("Too High");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Too Low");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input.");
+                }
+            }
+        }
+    }
+}
